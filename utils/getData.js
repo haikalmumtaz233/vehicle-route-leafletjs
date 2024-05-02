058aef0c26be6b83ccebafbba4a64e3d6b945215
Haikal Mumtaz <123210062@student.upnyk.ac.id>
2024-05-02 15:03:28 +0700

    sedikit update

--- utils/getData.js
+++ utils/getData.js
@@ -2,11 +2,9 @@ const nama = localStorage.getItem('nilai-nama');
 const no_hp = localStorage.getItem('nilai-no_hp');
 const asal = localStorage.getItem('nilai-asal');
 const tujuan = localStorage.getItem('nilai-tujuan');
-const plat = localStorage.getItem('nilai-plat');
 
 
 document.getElementById('nama').innerText = nama;
 document.getElementById('no_hp').innerText = no_hp;
-document.getElementById('plat').innerText = plat;
 document.getElementById('asal').innerText = asal;
 document.getElementById('tujuan').innerText = tujuan;
--- utils/processData.js
+++ utils/processData.js
@@ -3,7 +3,6 @@ const nama = document.getElementById('nama');
 const no_hp = document.getElementById('no_hp');
 const asal = document.getElementById('asal');
 const tujuan = document.getElementById('tujuan');
-const plat = document.getElementById('plat');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -12,13 +11,11 @@ form.addEventListener('submit', function(e) {
     const nilaiNomorHp = no_hp.value;
     const nilaiAsal = asal.value;
     const nilaiTujuan = tujuan.value;
-    const nilaiPlat = plat.value;
 
     localStorage.setItem('nilai-nama', nilaiNama);
     localStorage.setItem('nilai-no_hp', nilaiNomorHp);
     localStorage.setItem('nilai-asal', nilaiAsal);
     localStorage.setItem('nilai-tujuan', nilaiTujuan);
-    localStorage.setItem('nilai-plat', nilaiPlat);
 
     window.location.href = 'track.html';
 })
